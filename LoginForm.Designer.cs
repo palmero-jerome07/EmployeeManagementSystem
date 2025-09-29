f1db4eb90de658479811e179051901ac397f04a5
palmero-jerome07 <jeromepalmero7@gmail.com>
2025-09-29 11:04:13 +0800

    Added login form

---
+++ LoginForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace EmployeeManagementSystem
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblHeader = new Label();
+            lblUsername = new Label();
+            lblPassword = new Label();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            btnLogin = new Button();
+            linklblCreateAccount = new LinkLabel();
+            SuspendLayout();
+            // 
+            // lblHeader
+            // 
+            lblHeader.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            lblHeader.AutoSize = true;
+            lblHeader.Font = new Font("Segoe UI Semibold", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblHeader.Location = new Point(157, 10);
+            lblHeader.Name = "lblHeader";
+            lblHeader.Size = new Size(164, 86);
+            lblHeader.TabIndex = 0;
+            lblHeader.Text = "EMS";
+            // 
+            // lblUsername
+            // 
+            lblUsername.AutoSize = true;
+            lblUsername.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblUsername.Location = new Point(65, 109);
+            lblUsername.Name = "lblUsername";
+            lblUsername.Size = new Size(81, 21);
+            lblUsername.TabIndex = 1;
+            lblUsername.Text = "Username";
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblPassword.Location = new Point(70, 156);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(76, 21);
+            lblPassword.TabIndex = 3;
+            lblPassword.Text = "Password";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(152, 109);
+            txtUsername.MaxLength = 50;
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(262, 23);
+            txtUsername.TabIndex = 4;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(152, 156);
+            txtPassword.MaxLength = 50;
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(262, 23);
+            txtPassword.TabIndex = 5;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnLogin.Location = new Point(350, 195);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(64, 26);
+            btnLogin.TabIndex = 6;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // linklblCreateAccount
+            // 
+            linklblCreateAccount.AutoSize = true;
+            linklblCreateAccount.Font = new Font("Segoe UI", 9F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            linklblCreateAccount.Location = new Point(311, 224);
+            linklblCreateAccount.Name = "linklblCreateAccount";
+            linklblCreateAccount.Size = new Size(103, 15);
+            linklblCreateAccount.TabIndex = 7;
+            linklblCreateAccount.TabStop = true;
+            linklblCreateAccount.Text = "Create an account";
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(485, 267);
+            Controls.Add(linklblCreateAccount);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(lblPassword);
+            Controls.Add(lblUsername);
+            Controls.Add(lblHeader);
+            Name = "LoginForm";
+            Text = "LoginForm";
+            Load += LoginForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblHeader;
+        private Label lblUsername;
+        private Label lblPassword;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private Button btnLogin;
+        private LinkLabel linklblCreateAccount;
+    }
+}
---
+++ LoginForm.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Collections.Specialized.BitVector32;
+using static System.Windows.Forms.DataFormats;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace EmployeeManagementSystem
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        public string username, age, section;
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text.Trim();
+            string password = txtPassword.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(username) && string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Please enter both username and password.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else if (string.IsNullOrWhiteSpace(username))
+            {
+                MessageBox.Show("Please enter username.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+
+            }
+            else if (string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Please enter password.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                string sql = $"SELECT [FullName], [Section], [Age], [Username], [Password] FROM tblUsers WHERE Username = '{username}' AND Password = '{password}'";
+                bool isAuthenticated = CRUD.CRUD.RETRIEVESINGLE(sql);
+
+                string dbEmail, dbPassword;
+                string fullname, Section, Age;
+                int dtCount = CRUD.CRUD.dt.Rows.Count;
+                
+                if(isAuthenticated && dtCount > 0)
+                {
+                    dbEmail = CRUD.CRUD.dt.Rows[0]["Username"].ToString();
+                    dbPassword = CRUD.CRUD.dt.Rows[0]["Password"].ToString();
+
+
+                    if(dbEmail.Equals(username, StringComparison.Ordinal) && dbPassword.Equals(password, StringComparison.Ordinal))
+                    {
+                        fullname = CRUD.CRUD.dt.Rows[0]["FullName"].ToString();
+                        Section = CRUD.CRUD.dt.Rows[0]["Section"].ToString();
+                        Age = CRUD.CRUD.dt.Rows[0]["Age"].ToString();
+
+                        MessageBox.Show($"Welcome to EMS, {fullname}!", "Login Successfully");
+                        this.Hide();
+
+                        frmMotherForm openForm = new frmMotherForm();
+                        openForm.ShowDialog();
+                    } else
+                    {
+                        MessageBox.Show("Invalid username or password.", "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        return;
+                    }
+                } else
+                {
+                    MessageBox.Show("No data found.", "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+            }
+        }
+    }
+}
